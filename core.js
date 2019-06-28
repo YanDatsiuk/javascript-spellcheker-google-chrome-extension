a7a51d033dfb2d5de1723825602abe4b719cc9c2
Yan Datsiuk <yan@yandatsiuk.name>
2019-06-28 19:46:15 +0300

    added method for extracting dictionary from local storage

--- core.js
+++ core.js
@@ -1,2 +1,14 @@
 //todo add some code here 
 console.log("hello from call");
+
+//
+
+var spellc_webpage = function() {
+
+    //get dictionary from storage
+    chrome.storage.local.get(["spellc_dic_en_0"], function (result) {
+        console.log("ok2");
+        console.log(result);
+    });
+    console.log("ok");
+}
